e7559df032a7bccbc555cc45398806f2bafe01b5
= <=>
2021-10-21 14:03:01 +0200

    RouterLinks configurados, Componente lista hecho, componente detail hecho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent  } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'list', component: ListComponent},
+  {path: 'list/:heroeId', component: DetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,3 +1,4 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Heroe } from '../models/heroes.interface';
 import { ApiheroesService } from '../services/apiheroes.service';
@@ -9,6 +10,8 @@ import { ApiheroesService } from '../services/apiheroes.service';
 export class ListComponent implements OnInit {
   // COmo la API no ofrece paginacion de personajes, llamaremos todos de una sola vez
   heroes: Heroe[] = [];
+  page: number = 1;
+  limitReached: boolean = false;
 
   // Inyeccion de dependencia
   constructor(private apiheroesService: ApiheroesService) { }
@@ -31,7 +34,69 @@ export class ListComponent implements OnInit {
         this.heroes.push(newHeroe);
       });
     }
-    console.log(this.heroes)
+  }
+
+  loadNext(): void{
+    if(this.limitReached){
+      alert("Limite de heroes alcanzado");
+      return;
+    }
+    this.page += 1;
+    this.heroes =[];
+
+    const initialHeroe = (25*this.page - 25) +1;
+    const finalHeroe = 25*this.page;
+
+    for(let i=initialHeroe; i<=finalHeroe; i++){
+      this.apiheroesService.getCharactersById(i).
+      subscribe((data: any)=>{
+        if(data.response == "error"){
+          this.limitReached = true;
+          return;
+        }
+        const newHeroe: Heroe = {
+          id: data.id,
+          name: data.name,
+          biography: JSON.stringify(data.biography),
+          powerstats: JSON.stringify(data.powerstats),
+          appearance: JSON.stringify(data.appearance),
+          work: JSON.stringify(data.work),
+          connections: JSON.stringify(data.connections),
+          image: data.image.url
+        };
+        this.heroes.push(newHeroe);
+      });
+    }
+  }
+
+  loadBefore(): void{
+    if(this.page==1){
+      alert("Estamos en la primera pagina de heroes");
+      return;
+    }
+    this.page -= 1;
+    this.heroes =[];
+    this.limitReached = false;
+
+    const initialHeroe = (25*this.page - 25) +1;
+    const finalHeroe = 25*this.page;
+
+    for(let i=initialHeroe; i<=finalHeroe; i++){
+      this.apiheroesService.getCharactersById(i).
+      subscribe((data: any)=>{
+        const newHeroe: Heroe = {
+          id: data.id,
+          name: data.name,
+          biography: JSON.stringify(data.biography),
+          powerstats: JSON.stringify(data.powerstats),
+          appearance: JSON.stringify(data.appearance),
+          work: JSON.stringify(data.work),
+          connections: JSON.stringify(data.connections),
+          image: data.image.url
+        };
+        this.heroes.push(newHeroe);
+      });
+    }
   }
 
 }
